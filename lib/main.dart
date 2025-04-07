270a715f0189272ae27740db1e490ea650521068
abigailsheldon <abigail.l.sheldon@gmail.com>
2025-04-07 14:42:27 -0400

    Adds skeleton code to implement

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 
@@ -14,74 +16,84 @@ void main() async {
   runApp(MyApp());
 }
 
+class Task {
+  String id;
+  String name;
+  bool isCompleted;
+  String priority;
+  List<String>? subTasks;
+  // tasks/{taskId}/subtasks/{subtaskId}
+
+  Task({
+    required this.id,
+    required this.name,
+    this.isCompleted = false,
+    this.priority = 'Medium',
+  });
+
+  Map<String, dynamic> toMap() => {
+        'name': name,
+        'isCompleted': isCompleted,
+        'priority': priority,
+        'userId': FirebaseAuth.instance.currentUser?.uid,
+      };
+
+  factory Task.fromMap(String id, Map<String, dynamic> map) {
+    return Task(
+      id: id,
+      name: map['name'],
+      isCompleted: map['isCompleted'],
+      priority: map['priority'],
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Task Manager',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TaskListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-
-  final String title;
-
+class TaskListScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TaskListScreenState createState() => _TaskListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TaskListScreenState extends State<TaskListScreen> {
 
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
+
+  void _addTask(){
+
   }
 
-  @override
-  Widget build(BuildContext context) {
+  void _toggleTask(){
     
-    return Scaffold(
-      appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
+  }
+
+  void _deleteTask(){
+
+  }
+
+  Stream<List<Task>> _getTasks() {
+
+  }
+
+  Color _getPriorityColor(String priority) {
+  }
+
+  @override
+  Widget build(BuildContext context){
+
+
   }
 }
